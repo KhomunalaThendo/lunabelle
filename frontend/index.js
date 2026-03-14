1c899de88a841ac325e01487faa6ca025b5f595b
Thendo Khomunala <168510273+KhomunalaThendo@users.noreply.github.com>
2026-03-14 23:25:42 +0200

    Add files via upload

---
+++ frontend/index.js
@@ -0,0 +1,34 @@
+/*
+This is a list of products (array with all the products) the store is selling at the moment
+*/
+
+const products = [
+{
+    id: 1,
+    prodPicture: './src/category1.png',
+    title: 'La Virtus',
+    price: 170.00,
+    category: 'bags' 
+},
+{
+    id: 2,
+    title: 'La Cura',
+    price: 60.00,
+    category: 'selfCare'
+},
+{
+    id: 3,
+    prodPicture: './src/tight-top.jpg',
+    title: 'La Flexa Top',
+    price: 75.00,
+    category: 'clothing'
+},
+{
+    id: 4,
+    prodPicture: './src/perfume.jpg',
+    title: 'La Audacia',
+    price: 100,
+    category: 'selfCare'
+}
+];
+
